99b0dc75340d68fa1724c5aa64f2c93b80b01b1a
Sia Bhatia <bhatiasia@gmail.com>
2021-07-28 02:45:26 +0000

    testt

--- day12/starter/firebaseNotes/Day12/public/js/writeNote.js
+++ day12/starter/firebaseNotes/Day12/public/js/writeNote.js
@@ -3,7 +3,7 @@ let googleUser;
 window.onload = (event) => {
   // Use this to retain user state between html pages.
   firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
+    if (user) { 
       console.log('Logged in as: ' + user.displayName);
       googleUser = user;
     } else {
